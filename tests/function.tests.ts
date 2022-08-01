7c8d6bf108c1477f7a0eba1c726ec3d8a07f34b4
maziac <thomas.busse@gmx.net>
2022-08-01 18:01:20 +0200

    Added mocha framework.

---
+++ tests/function.tests.ts
@@ -0,0 +1,22 @@
+import * as assert from 'assert';
+
+/**
+ * This tests the functions defined in the webview.
+ * Simply copy the functions here to test.
+ * E.g. copy function 'getNumberValue(...)' here.
+ */
+
+
+suite('Functions', () => {
+
+	suite('getNumbervalue()', () => {
+
+		// Called for each test.
+		setup(() => {
+		});
+
+		test('simple', () => {
+			assert.equal(1, 0);
+		});
+	});
+});
